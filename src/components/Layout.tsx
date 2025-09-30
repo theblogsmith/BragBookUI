209fb0a364a249f20e00cf36ef3cdf75102a4a1e
theblogsmith <113307503+theblogsmith@users.noreply.github.com>
2025-09-30 16:29:45 -0600

    Updated Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Sidebar from './Sidebar';
-const Layout = () => {
-  return <div className="flex flex-col h-screen bg-white">
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="flex flex-col h-screen bg-white">
       <Header />
       <div className="flex flex-1 overflow-hidden">
         <Sidebar />
         <main className="flex-1 overflow-y-auto p-4 md:p-6 bg-white">
-          <Outlet />
+          {children}
         </main>
       </div>
-    </div>;
+    </div>
+  );
 };
+
 export default Layout;
